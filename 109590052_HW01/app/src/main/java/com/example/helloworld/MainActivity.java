e3c2add3651a29ae4ac51e6dd2d9c17371e6578f
Benson0406 <love330320254@gmail.com>
2022-03-14 22:43:12 +0800

    HW3

--- 109590052_HW01/app/src/main/java/com/example/helloworld/MainActivity.java
+++ 109590052_HW01/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,16 +1,51 @@
-package com.example.helloworld;
+
+package com.example.android.helloconstraint;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello World");
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mCount % 2 == 0){
+            view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }else{
+            view.setBackgroundColor(0xFF5F1F00);
+        }
+        if (mCount != 0){
+            Button button = (Button) findViewById(R.id.button_zero);
+            button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        }
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void countZero(View view){
+        mCount = 0;
+        view.setBackgroundColor(getResources().getColor(R.color.colorGray));
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
